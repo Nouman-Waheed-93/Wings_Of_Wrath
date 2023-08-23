1c1acbd776670c1a007edeee8d3f8c238a47500b
Nouman Waheed <nouman1993@live.com>
2023-08-23 12:53:33 +0500

    PositionIndicator.cs cleaned up
    Unused variables removed

--- Assets/Scripts/Common/PositionIndicator.cs
+++ Assets/Scripts/Common/PositionIndicator.cs
@@ -9,26 +9,17 @@ namespace Common
 
         public Transform Target { get => target; set => target = value; }
 
-
-        private RectTransform canvas;
         private Camera cam;
 
-        private RectTransform rectTransform;
-        private Vector3 screenCentre;
-        
         private void Awake()
         {
-            rectTransform = (RectTransform)transform;
             cam = Camera.main;
-            canvas = GetComponentInParent<Canvas>().GetComponent<RectTransform>(); 
-            screenCentre = new Vector3(Screen.width, Screen.height, 0) / 2;
         }
 
         private void Update()
         {
             Vector3 newPos = cam.WorldToScreenPoint(target.position);
-            float angle = 0;
-            //GetArrowIndicatorPositionAndAngle(ref newPos, ref angle, screenCentre, screenCentre * 0.9f);
+            
             if (newPos.z < 0)
                 newPos *= -1;
 
