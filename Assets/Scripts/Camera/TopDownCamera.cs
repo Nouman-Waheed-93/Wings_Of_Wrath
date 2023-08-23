e4d0d3f87b39edf1fce8104cb6739762cb566e0d
Nouman Waheed <nouman1993@live.com>
2023-08-23 18:55:43 +0500

    Unneeded changes removed

--- Assets/Scripts/Camera/TopDownCamera.cs
+++ Assets/Scripts/Camera/TopDownCamera.cs
@@ -8,11 +8,6 @@ namespace CameraController
         private Vector3 offset;
         [SerializeField]
         private Transform target;
-        [SerializeField]
-        private float followSmoothTime = 0.25f;
-        [SerializeField]
-        private float rotationSpeedMultiplier = 1;
-        private Vector3 currVelocity;
 
         private void LateUpdate()
         {
@@ -22,14 +17,12 @@ namespace CameraController
 
         private void SetPosition()
         {
-            transform.position = Vector3.SmoothDamp(transform.position, 
-                target.position + (target.forward * offset.z) + new Vector3(0, offset.y, 0), 
-                ref currVelocity, followSmoothTime);
+            transform.position = target.position + (target.forward * offset.z) + new Vector3(0, offset.y, 0);
         }
 
         private void SetRotation()
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, target.eulerAngles.y, 0), rotationSpeedMultiplier * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(0, target.eulerAngles.y, 0);
         }
     }
 }
