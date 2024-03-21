a755ad7e5f6cf8caafab3ba9b449f0052781a5cf
Nouman Waheed <nouman1993@live.com>
2024-03-21 14:17:46 +0900

    NPC aircraft can control their speed while in a formation now

--- Assets/Scripts/AircraftController/Ai/AircraftAIController.cs
+++ Assets/Scripts/AircraftController/Ai/AircraftAIController.cs
@@ -18,29 +18,37 @@ namespace AircraftController
             public StateFollowWaypoints stateFollowWaypoints { get; private set; }
             public StateFollowFormation stateFollowFormation { get; private set; }
 
-            private PIDController speedPIDController;
-            public PIDController SpeedPIDController { get => speedPIDController; }
-
             public bool IsAfterBurnerOn => true;
 
             private float turnInput;
             private float desiredSpeed;
 
+            private float speedError;
+            private float errorSetTime;
+
             public AircraftAIController(IAircraft aircraft, IRelativePositionProvider transform, Vector3[] wayPoints)
             {
                 this.aircraft = aircraft;
                 this.transform = transform;
-                speedPIDController = new PIDController();
-                speedPIDController.minimum = -1;
-                speedPIDController.maximum = 0;
                 stateFollowWaypoints = new StateFollowWaypoints(stateMachine, this, wayPoints);
                 stateFollowFormation = new StateFollowFormation(stateMachine, this);
                 stateMachine.Initialize(stateFollowWaypoints);
             }
 
+            private void RandomizeSpeedError()
+            {
+                speedError = Random.Range(-2, 2);
+            }
+
             public void Update(float simulationDeltaTime)
             {
                 stateMachine.currentState.Update(simulationDeltaTime);
+                errorSetTime += simulationDeltaTime;
+                if(errorSetTime > 1)
+                {
+                    errorSetTime = 0;
+                    RandomizeSpeedError();
+                }
             }
 
             public float GetDesiredSpeed()
@@ -75,14 +83,14 @@ namespace AircraftController
                 Debug.DrawLine(transform.position, targetPosition, Color.red);
                 Arrive(targetPosition);
 
-                float dot = Vector3.Dot(transform.forward, targetPosition - transform.position);
-                bool isTargetBehind = dot < -0.75f;
+                //float dot = Vector3.Dot(transform.forward, targetPosition - transform.position);
+                //bool isTargetBehind = dot < -0.75f;
                 float distance = Vector3.Distance(transform.position, targetPosition);
-                if (isTargetBehind && distance < 100)
-                {
-                    targetPosition = transform.position + transform.forward;
-                 //   desiredSpeed = aircraft.MovementHandler.AerodynamicMovementData.lowAirSpeed;
-                }
+                //if (isTargetBehind && distance < 100)
+                //{
+                //    targetPosition = transform.position + transform.forward;
+                // //   desiredSpeed = aircraft.MovementHandler.AerodynamicMovementData.lowAirSpeed;
+                //}
 
                 targetPosition += leader.Transform.forward * desiredSpeed;
 
@@ -98,11 +106,26 @@ namespace AircraftController
             {
                 Vector3 ToPosition = targetPosition - transform.position;
 
-                float distanceAhead = Vector3.Dot(ToPosition, transform.forward) / 20;
-                float pidVal = speedPIDController.Seek(0, distanceAhead);
-                desiredSpeed = pidVal * -1 * aircraft.MovementHandler.AerodynamicMovementData.highAirSpeed;
-                desiredSpeed = Mathf.Clamp(desiredSpeed, aircraft.MovementHandler.AerodynamicMovementData.lowAirSpeed, aircraft.MovementHandler.AerodynamicMovementData.highAirSpeed);
-                Debug.Log("PIDVal " + pidVal + "Dist : " + distanceAhead + " desiredSpeed : " + desiredSpeed);
+                float distanceAhead = Vector3.Dot(ToPosition, transform.forward);
+
+                if (distanceAhead > 0)
+                {
+                    float lerpVal = distanceAhead / 200;
+                    desiredSpeed = Mathf.Lerp(aircraft.MovementHandler.AerodynamicMovementData.normalAirSpeed, aircraft.MovementHandler.AerodynamicMovementData.highAirSpeed, lerpVal);
+                    desiredSpeed += speedError;
+                    desiredSpeed = Mathf.Clamp(desiredSpeed, aircraft.MovementHandler.AerodynamicMovementData.lowAirSpeed, aircraft.MovementHandler.AerodynamicMovementData.highAirSpeed);
+                    Debug.Log("Lerp val is " + lerpVal + " desiredSpeed " + desiredSpeed);
+                }
+                else
+                {
+                    float lerpVal = (distanceAhead * -1) / 100;
+                    lerpVal = Mathf.Clamp01(lerpVal);
+                    lerpVal = 1 - lerpVal;
+                    desiredSpeed = Mathf.Lerp(aircraft.MovementHandler.AerodynamicMovementData.lowAirSpeed, aircraft.MovementHandler.AerodynamicMovementData.normalAirSpeed, lerpVal);
+                    desiredSpeed += speedError;
+                    desiredSpeed = Mathf.Clamp(desiredSpeed, aircraft.MovementHandler.AerodynamicMovementData.lowAirSpeed, aircraft.MovementHandler.AerodynamicMovementData.highAirSpeed);
+                    Debug.Log("reverse Lerp val is " + lerpVal + " desiredSpeed " + desiredSpeed);
+                }
             }
         }
     }
